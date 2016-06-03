00b7282e60c0710089080369abe49c14aed2a444
cc@192.168.1.151 <cc@192.168.1.151>
2016-06-03 18:30:04 +0800

    11

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+print BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -23,6 +23,10 @@ SECRET_KEY = 'gdwij-7+zc%73h2yjysrmc++5)&68(jw47t+8yi7syq%0*6hmd'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+TEMPLATE_DIRS = {
+        os.path.join(BASE_DIR, 'templates')
+        }
 
 ALLOWED_HOSTS = []
 
