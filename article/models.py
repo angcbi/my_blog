75d6c2abad5aae040e972003346f056cfec6c0b8
cc@192.168.1.151 <cc@192.168.1.151>
2016-06-07 17:57:19 +0800

    1

--- article/models.py
+++ article/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Article(models.Model):
     title = models.CharField(max_length = 100)
     category = models.CharField(max_length = 50, blank = True)
     date_time = models.DateTimeField(auto_now_add = True)
     content = models.TextField(blank = True, null = True)
+
+    def get_absolute_url(self):
+        path = reverse('detail', args=(self.id,))
+        return 'http://127.0.0.1:8000{}'.format(path)
 
     def __unicode__(self):
         return self.title
