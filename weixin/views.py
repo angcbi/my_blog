22f7a2a486928e37f0b444d9ab4ba75311ecf803
cc@192.168.1.151 <cc@192.168.1.151>
2016-06-21 18:41:34 +0800

    1

--- weixin/views.py
+++ weixin/views.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # encoding:utf-8
 
-import socket
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils import check_Signature
 
 def checkSignature(request):
-    print request.method
-    signature = request.GET.get('signature')
-    timestamp = request.GET.get('timestamp')
-    nonce = request.GET.get('nonce')
-    echostr = request.GET.get('echostr')
-    print socket.gethostname()
+    if request.method == 'GET':
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        echostr = request.GET.get('echostr')
 
     if check_Signature(signature=signature, timestamp=timestamp, nonce=nonce):
         return HttpResponse(echostr)
 
-    return HttpResponse('')
-
+    print request.POST
